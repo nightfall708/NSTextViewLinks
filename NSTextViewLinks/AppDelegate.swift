c6c9ebd986a87b99d765fc8e88720cb62a1eeadb
Tal Shrestha <talshrestha@gmail.com>
2017-05-17 20:42:20 +0300

    Initial Commit

---
+++ NSTextViewLinks/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSTextViewLinks
+//
+//  Created by Tal Shrestha on 17/05/2017.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NSTextViewLinks/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSTextViewLinks
+//
+//  Created by Tal Shrestha on 17/05/2017.
+//  Copyright © 2017 None. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
